140ec8ace7719347143697462c04a8dc5d8b4d90
Luke Vang <lukenvang@gmail.com>
2023-08-29 15:25:49 -0500

    derp

--- index.js
+++ index.js
@@ -2,20 +2,62 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  test();
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+    // `who-to-greet` input defined in action metadata file
+    const actionToRun = core.getInput('action-to-run');
+    console.log(`Running ${actionToRun} action`);
+    switch (actionToRun) {
+        case 'set-git-credentials':
+            setGitCredentials();
+        case 'release-check':
+            releaseCheck();
+        case 'approvals-check':
+            approvalsCheck();
+        case 'check-for-conflicts':
+            checkForConflicts();
+        case 'get-diffs':
+            getDiffs();
+        case 'generate-release-package':
+            generateReleasePackage();
+        case 'authorize-into-salesforce':
+            authorizeIntoSalesforce();
+        case 'validation-changes':
+            validationChanges();
+        default:
+    }
 } catch (error) {
-  core.setFailed(error.message);
+    core.setFailed(error.message);
 }
 
-function test()
-{
-    console.log("sadfaslkasmdfl;asdkmf ;klasdmfkl;amsdl;fkma sl;dkfm akl;smf kl;asdmf;klasdmf s");
+function setGitCredentials() {
+    console.log('setGitCredentials');
+}
+
+function releaseCheck(releaseString) {
+    if(!releaseString) throw new Error('releaseString is required');
+    console.log('releaseString');
+}
+
+function approvalsCheck(minimumApprovals) {
+    if(!minimumApprovals) throw new Error('minimumApprovals is required');
+    console.log('approvalsCheck');
+}
+
+function checkForConflicts() {
+    console.log('checkForConflicts');
+}
+
+function getDiffs() {
+    console.log('getDiffs');
+}
+
+function generateReleasePackage() {
+    console.log('generateReleasePackage');
+}
+
+function authorizeIntoSalesforce() {
+    console.log('authorizeIntoSalesforce');
+}
+
+function validationChanges() {
+    console.log('validationChanges');
 }
