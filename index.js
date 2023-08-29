5fbe618d16885c7b348e78eb94957ddcc291479e
Luke Vang <lukenvang@gmail.com>
2023-08-29 14:54:52 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,18 +4,18 @@ const github = require('@actions/github');
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
+  test();
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
-  test();
 } catch (error) {
   core.setFailed(error.message);
 }
 
-async function test()
+function test()
 {
-    console.log("test");
+    console.log("sadfaslkasmdfl;asdkmf ;klasdmfkl;amsdl;fkma sl;dkfm akl;smf kl;asdmf;klasdmf s");
 }
